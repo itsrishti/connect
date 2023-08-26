c6f5b7b5ebc217f1e2986bc16280e542b5b96eca
itsrishti <123953052+itsrishti@users.noreply.github.com>
2023-08-27 01:36:55 +0530

    aadhar

--- src/App.js
+++ src/App.js
@@ -94,21 +94,24 @@ function App () {
                                     <input type="text" className="form-control" value="(320) 380-4539"/>
                                 </div>
                             </div>
-                            <div className="row mb-3">
-                                <div className="col-sm-3">
-                                    <h6 className="mb-0">Mobile</h6>
-                                </div>
-                                <div className="col-sm-9 text-secondary">
-                                    <input type="text" className="form-control" value="(320) 459-6547"/>
-                                </div>
-                            </div>
+                            
                             <div className="row mb-3">
                                 <div className="col-sm-3">
                                     <h6 className="mb-0">Address</h6>
                                 </div>
-                                <div class="col-sm-9 text-secondary">
-                                    <input type="text" className="form-control" value="Bay Area, San Francisco, CA"/>
+                                <div className="col-sm-9 text-secondary">
+                                    <input type="text" className="form-control" value="BayArea, SanFrancisco,CA"/>
                                 </div>
+                                
+                            </div>
+                            <div className="row mb-3">
+                                <div className="col-sm-3">
+                                    <h6 className="mb-0">Documents</h6>
+                                </div>
+                                <div className="col-sm-9 text-secondary">
+                                    <input type="text" className="form-control" value="Aadhar Card"/>
+                                </div>
+                              
                             </div>
                             <div className="row">
                                 <div className="col-sm-3"></div>
