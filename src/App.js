23ebb475dc98594df858440b073ac2dada9a11b9
itsrishti <123953052+itsrishti@users.noreply.github.com>
2023-08-27 00:56:03 +0530

    colour change

--- src/App.js
+++ src/App.js
@@ -15,57 +15,67 @@ function App () {
 
     
 
-    <div className="container">
-        <div className="main-body">
+    <div className="container ">
+        <div className="main-body bg-body-secondary">
             <div className="row">
                 <div className="col-lg-4">
-                    <div className="card">
-                        <div className="card-body ">
+                    <div className="card bg-body-secondary ">
+                        <div className="card-body bg-body-secondary ">
                             <div className="d-flex flex-column align-items-center text-center">
                                 <img src="https://bootdey.com/img/Content/avatar/avatar6.png" alt="Admin" className="rounded-circle p-1 bg-primary" width="110"/>
                                 <div className="mt-3">
-                                    <h4>John Doe</h4>
-                                    <p className="text-secondary mb-1">Full Stack Developer</p>
+                                    <h4>Shiv Mehra</h4>
+                                    <p className="text-secondary mb-1">Doctor</p>
                                     <p className="text-muted font-size-sm">Bay Area, San Francisco, CA</p>
-                                    <button className="btn btn-primary">Follow</button>
+                                    <button className="btn btn-primary ">Follow</button>
                                     <button className="btn btn-outline-primary">Message</button>
                                 </div>
                             </div>
-                            <hr className="my-4"/>
-                            <ul className="list-group list-group-flush">
+                            <hr className="my-4 "/>
+                            <ul className="list-group list-group-flush ">
                                 <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                    <h6 className="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-globe me-2 icon-inline"><circle cx="12" cy="12" r="10"></circle><line x1="2" y1="12" x2="22" y2="12"></line><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg>Website</h6>
-                                    <span className="text-secondary">https://bootdey.com</span>
+                                    <h6 className="mb-0">Age</h6>
+                                    <span className="text-secondary">28</span>
                                 </li>
                                 <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                                     <h6 className="mb-0">weight</h6>
                                     <span className="text-secondary">64kg</span>
                                 </li>
                                 <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                    <h6 className="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter me-2 icon-inline text-info"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>Twitter</h6>
-                                    <span className="text-secondary">@bootdey</span>
+                                    <h6 className="mb-0">Height</h6>
+                                    <span className="text-secondary">6'1</span>
                                 </li>
                                 <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                    <h6 className="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-instagram me-2 icon-inline text-danger"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>Instagram</h6>
-                                    <span className="text-secondary">bootdey</span>
+                                    <h6 className="mb-0">Gender</h6>
+                                    <span className="text-secondary">Male</span>
                                 </li>
                                 <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                    <h6 className="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook me-2 icon-inline text-primary"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>Facebook</h6>
-                                    <span className="text-secondary">bootdey</span>
+                                    <h6 className="mb-0">Blood Type</h6>
+                                    <span className="text-secondary">A+</span>
+                                </li>
+                                <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                                    <h6 className="mb-0">Blood Pressure</h6>
+                                    <span className="text-secondary">120/80 mm Hg</span>
+                                </li><li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                                    <h6 className="mb-0">Pulse Rate</h6>
+                                    <span className="text-secondary">80 bpm</span>
+                                </li><li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                                    <h6 className="mb-0">Date of Last Blood Donation Date</h6>
+                                    <span className="text-secondary">01/09/23</span>
                                 </li>
                             </ul>
                         </div>
                     </div>
                 </div>
                 <div className="col-lg-8">
-                    {/* <div className="card">
+                     <div className="card"> 
                         <div className="card-body">
                             <div className="row mb-3">
                                 <div className="col-sm-3">
                                     <h6 className="mb-0">Full Name</h6>
                                 </div>
                                 <div className="col-sm-9 text-secondary">
-                                    <input type="text" className="form-control" value="John Doe"/>
+                                    <input type="text" className="form-control" value="Shiv Mehra"/>
                                 </div>
                             </div>
                             <div className="row mb-3">
@@ -73,7 +83,7 @@ function App () {
                                     <h6 className="mb-0">Email</h6>
                                 </div>
                                 <div className="col-sm-9 text-secondary">
-                                    <input type="text" className="form-control" value="john@example.com"/>
+                                    <input type="text" className="form-control" value="shiv@gmail.com"/>
                                 </div>
                             </div>
                             <div className="row mb-3">
@@ -81,7 +91,7 @@ function App () {
                                     <h6 className="mb-0">Phone</h6>
                                 </div>
                                 <div class="col-sm-9 text-secondary">
-                                    <input type="text" className="form-control" value="(239) 816-9029"/>
+                                    <input type="text" className="form-control" value="(320) 380-4539"/>
                                 </div>
                             </div>
                             <div className="row mb-3">
@@ -89,7 +99,7 @@ function App () {
                                     <h6 className="mb-0">Mobile</h6>
                                 </div>
                                 <div className="col-sm-9 text-secondary">
-                                    <input type="text" className="form-control" value="(320) 380-4539"/>
+                                    <input type="text" className="form-control" value="(320) 459-6547"/>
                                 </div>
                             </div>
                             <div className="row mb-3">
@@ -103,33 +113,33 @@ function App () {
                             <div className="row">
                                 <div className="col-sm-3"></div>
                                 <div className="col-sm-9 text-secondary">
-                                    <input type="button" className="btn btn-primary px-4" value="Save Changes"/>
+                                    <input type="button " className="btn btn-primary px-4" value="Save Changes"/>
                                 </div>
                             </div>
                         </div>
-                    </div> */}
+                    </div> 
                     <div className="row">
                         <div className="col-sm-12">
                             <div className="card">
                                 <div className="card-body">
                                     <h5 className="d-flex align-items-center mb-3">HEALTH STATUS</h5>
-                                    <p>Web Design</p>
+                                    <p>Allergies</p>
                                     <div className="progress mb-3" style={{height: '5px'}}>
                                         <div className="progress-bar bg-primary" role="progressbar" style={{width: '80%'}} aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
                                     </div>
-                                    <p>Website Markup</p>
+                                    <p>Blood Infection</p>
                                     <div className="progress mb-3" style={{height: '5px'}}>
                                         <div className="progress-bar bg-danger" role="progressbar" style={{width:'72%'}} aria-valuenow="72" aria-valuemin="0" aria-valuemax="100"></div>
                                     </div>
-                                    <p>One Page</p>
+                                    <p>Surgery</p>
                                     <div className="progress mb-3" style={{height: '5px'}}>
                                         <div className="progress-bar bg-success" role="progressbar" style={{width:' 89%'}} aria-valuenow="89" aria-valuemin="0" aria-valuemax="100"></div>
                                     </div>
-                                    <p>Mobile Template</p>
+                                    <p>Plasma Requirement</p>
                                     <div className="progress mb-3" style={{height: '5px'}}>
                                         <div className="progress-bar bg-warning" role="progressbar" style={{width: '55%'}} aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
                                     </div>
-                                    <p>Backend API</p>
+                                    <p>Diseases</p>
                                     <div className="progress" style={{height: '5px'}}>
                                         <div className="progress-bar bg-info" role="progressbar" style={{width:' 66%'}} aria-valuenow="66" aria-valuemin="0" aria-valuemax="100"></div>
                                     </div>
