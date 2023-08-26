b79ebd8458a1cd37b21bd8f44b6fa9a853938bf3
itsrishti <123953052+itsrishti@users.noreply.github.com>
2023-08-26 21:32:15 +0530

    css

--- src/App.js
+++ src/App.js
@@ -108,11 +108,11 @@ function App() {
                                     <h5 className="d-flex align-items-center mb-3">Project Status</h5>
                                     <p>Web Design</p>
                                     <div className="progress mb-3" style={{height: '5px'}}>
-                                        <div className="progress-bar bg-primary" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                                        <div className="progress-bar bg-primary" role="progressbar" style={{width: '80%'}} aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
                                     </div>
                                     <p>Website Markup</p>
                                     <div className="progress mb-3" style={{height: '5px'}}>
-                                        <div className="progress-bar bg-danger" role="progressbar" style="width: 72%" aria-valuenow="72" aria-valuemin="0" aria-valuemax="100"></div>
+                                        <div className="progress-bar bg-danger" role="progressbar" style={{width:'72%'}} aria-valuenow="72" aria-valuemin="0" aria-valuemax="100"></div>
                                     </div>
                                     <p>One Page</p>
                                     <div className="progress mb-3" style={{height: '5px'}}>
