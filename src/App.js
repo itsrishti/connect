bfd5066d7e5cd386d935f13b67214c95d29c4575
itsrishti <123953052+itsrishti@users.noreply.github.com>
2023-08-26 22:32:17 +0530

    weight

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App () {
+
+
+        
+    
+      
+          
+
   return (
-<div>
+<div className="bg-danger">
     
 
     
@@ -13,7 +20,7 @@ function App() {
             <div className="row">
                 <div className="col-lg-4">
                     <div className="card">
-                        <div className="card-body">
+                        <div className="card-body ">
                             <div className="d-flex flex-column align-items-center text-center">
                                 <img src="https://bootdey.com/img/Content/avatar/avatar6.png" alt="Admin" className="rounded-circle p-1 bg-primary" width="110"/>
                                 <div className="mt-3">
@@ -31,8 +38,8 @@ function App() {
                                     <span className="text-secondary">https://bootdey.com</span>
                                 </li>
                                 <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                    <h6 className="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github me-2 icon-inline"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>Github</h6>
-                                    <span className="text-secondary">bootdey</span>
+                                    <h6 className="mb-0">weight</h6>
+                                    <span className="text-secondary">64kg</span>
                                 </li>
                                 <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                                     <h6 className="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter me-2 icon-inline text-info"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>Twitter</h6>
@@ -51,7 +58,7 @@ function App() {
                     </div>
                 </div>
                 <div className="col-lg-8">
-                    <div className="card">
+                    {/* <div className="card">
                         <div className="card-body">
                             <div className="row mb-3">
                                 <div className="col-sm-3">
@@ -100,12 +107,12 @@ function App() {
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div> */}
                     <div className="row">
                         <div className="col-sm-12">
                             <div className="card">
                                 <div className="card-body">
-                                    <h5 className="d-flex align-items-center mb-3">Project Status</h5>
+                                    <h5 className="d-flex align-items-center mb-3">HEALTH STATUS</h5>
                                     <p>Web Design</p>
                                     <div className="progress mb-3" style={{height: '5px'}}>
                                         <div className="progress-bar bg-primary" role="progressbar" style={{width: '80%'}} aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
